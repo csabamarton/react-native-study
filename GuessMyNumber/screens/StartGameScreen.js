6a1bfcfaa446659d31672e3fc603098f86bcacff
csabamarton <martoncsab@gmail.com>
2023-05-25 21:16:30 +0200

    Adding input validation

--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,7 +1,31 @@
-import {TextInput, View, StyleSheet} from "react-native";
+import {TextInput, View, StyleSheet, Alert} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import {useState} from "react";
 
 function StartGameScreen() {
+    const [enteredNumber, setEnteredNumber] = useState('')
+
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText)
+    }
+
+    function resetInputHAndler() {
+        setEnteredNumber('');
+    }
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber);
+
+        if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert(
+                'Invalid number!',
+                'Number has to be a number between 1 and 99',
+                [{text: 'Okay', style: 'destructive', onPress: resetInputHAndler}]);
+            return;
+        }
+
+        console.log('Valid number');
+    }
+
     return (
         <View style={styles.inputContainer}>
             <TextInput
@@ -10,13 +34,15 @@ function StartGameScreen() {
                 keyboardType={"number-pad"}
                   autoCapitalize={"none"}
                 autoCorrect={false}
+                value={enteredNumber}
+                onChangeText={numberInputHandler}
             />
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton onPress={resetInputHAndler}>Reset</PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Confirm</PrimaryButton>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
                 </View>
             </View>
         </View>
