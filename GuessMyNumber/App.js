addf3ef0678ee939aa6a0e5cb3b1c0758a832928
csabamarton <martoncsab@gmail.com>
2023-05-25 12:42:39 +0200

    Setting up Basic components and screens

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <StatusBar style='light' />
+            <View style={styles.container}>
+                <StartGameScreen />
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
 });
---
+++ GuessMyNumber/components/PrimaryButton.js
@@ -0,0 +1,32 @@
+import {Pressable, Text, View, StyleSheet} from "react-native";
+
+function PrimaryButton({children}) {
+
+    function pressHandler() {
+        console.log("Button got pressed")
+    }
+
+    return (<View style={styles.buttonOuterContainer}>
+        <Pressable
+            onPress={pressHandler}
+            android_ripple={{color: '#640233'}}
+            style={({pressed}) =>
+                pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}>
+            <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
+    </View>)
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28, margin: 4, overflow: "hidden"
+    }, buttonInnerContainer: {
+        backgroundColor: '#72063c', paddingVertical: 8, paddingHorizontal: 16, elevation: 2,
+    }, buttonText: {
+        color: 'white', textAlign: "center"
+    }, pressed: {
+        opacity: 0.75
+    }
+})
