6be89089678e5282c13f57e4ba196cdcad77a087
csabamarton <martoncsab@gmail.com>
2023-05-26 15:53:22 +0200

    - Adding GameScreen - Handling switch between screens - Adding new custom component: Title

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,8 +1,23 @@
-import {ImageBackground, StatusBar, StyleSheet, View} from 'react-native';
+import {ImageBackground, StatusBar, StyleSheet, View, SafeAreaView} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import {LinearGradient} from "expo-linear-gradient";
+import {useState} from "react";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+    const [userNumber, setUserNumber] = useState();
+
+    function pickedNumberHandler(pickedNumber) {
+        setUserNumber(pickedNumber)
+    }
+
+    let screen = <StartGameScreen onPickedNumber={pickedNumberHandler}/>
+
+    if (userNumber) {
+        console.log("Switch to GameScreen");
+        screen = <GameScreen/>;
+    }
+
     return (
         <LinearGradient style={styles.rootScreen} colors={['#4e0329', '#ddb52f']}>
             <ImageBackground
@@ -10,7 +25,9 @@ export default function App() {
                 style={styles.rootScreen}
                 imageStyle={styles.backgroundImage}
                 source={require('./assets/images/background.png')}>
-                <StartGameScreen/>
+                <SafeAreaView style={styles.rootScreen}>
+                    {screen}
+                </SafeAreaView>
             </ImageBackground>
         </LinearGradient>
     );
---
+++ GuessMyNumber/screens/GameScreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Text, View, StyleSheet} from "react-native";
+import Title from "../components/Title";
+
+function GameScreen() {
+    return (
+        <View style={styles.screen}>
+            <Title>Oponent's Guess</Title>
+
+            <View>
+                <Text>Higher or lower?</Text>
+            </View>
+            <View></View>
+        </View>
+
+    )
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    }
+})
+
+
